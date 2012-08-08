b0c97975894a5eebebf9d93147cdd941a3accb63
fbarchard@google.com <fbarchard@google.com@16f28f9a-4ce2-e073-06de-1de4eb20be90>
2012-08-08 19:04:24 +0000

    Update Copyright notice to follow new chromium conventions. BUG=63 TEST=none Review URL: https://webrtc-codereview.appspot.com/730004
    git-svn-id: http://libyuv.googlecode.com/svn/trunk@314 16f28f9a-4ce2-e073-06de-1de4eb20be90

--- include/libyuv/version.h
+++ include/libyuv/version.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2012 The LibYuv project authors. All Rights Reserved.
+ *  Copyright 2012 The LibYuv Project Authors. All rights reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -11,6 +11,6 @@
 #ifndef INCLUDE_LIBYUV_VERSION_H_  // NOLINT
 #define INCLUDE_LIBYUV_VERSION_H_
 
-#define LIBYUV_VERSION 313
+#define LIBYUV_VERSION 314
 
 #endif  // INCLUDE_LIBYUV_VERSION_H_  NOLINT
