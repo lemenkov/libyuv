f6e4e14713db8651d170e4b32171028264528e7a
fbarchard@google.com <fbarchard@google.com@16f28f9a-4ce2-e073-06de-1de4eb20be90>
2012-10-09 00:41:57 +0000

    .neon suffix on neon files BUG=108 TEST=none Review URL: https://webrtc-codereview.appspot.com/867007
    git-svn-id: http://libyuv.googlecode.com/svn/trunk@397 16f28f9a-4ce2-e073-06de-1de4eb20be90

--- Android.mk
+++ Android.mk
@@ -22,10 +22,10 @@ LOCAL_SRC_FILES := \
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_CFLAGS += -DLIBYUV_NEON
     LOCAL_SRC_FILES += \
-        source/compare_neon.cc \
+        source/compare_neon.cc.neon \
         source/rotate_neon.cc.neon \
         source/row_neon.cc.neon \
-        source/scale_neon.cc
+        source/scale_neon.cc.neon
 endif
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
