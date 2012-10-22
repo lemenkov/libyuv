95eb5264a6d03f8b7dc9180a379f8d1f077c5417
fbarchard@google.com <fbarchard@google.com@16f28f9a-4ce2-e073-06de-1de4eb20be90>
2012-10-22 19:55:40 +0000

    quick space removal fix for android.mk BUG=none TEST=none Review URL: https://webrtc-codereview.appspot.com/918006
    git-svn-id: http://libyuv.googlecode.com/svn/trunk@436 16f28f9a-4ce2-e073-06de-1de4eb20be90

--- Android.mk
+++ Android.mk
@@ -6,25 +6,25 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cc
 
 LOCAL_SRC_FILES := \
-    source/compare.cc           \ 
-    source/compare_common.cc    \ 
-    source/compare_posix.cc     \ 
-    source/convert.cc           \ 
-    source/convert_argb.cc      \ 
-    source/convert_from.cc      \ 
-    source/convert_from_argb.cc \ 
-    source/cpu_id.cc            \ 
-    source/format_conversion.cc \ 
-    source/planar_functions.cc  \ 
-    source/rotate.cc            \ 
-    source/rotate_argb.cc       \ 
-    source/row_common.cc        \ 
-    source/row_mips.cc          \ 
-    source/row_posix.cc         \ 
-    source/scale.cc             \ 
-    source/scale_argb.cc        \ 
-    source/scale_mips.cc        \ 
-    source/video_common.cc      \ 
+    source/compare.cc           \
+    source/compare_common.cc    \
+    source/compare_posix.cc     \
+    source/convert.cc           \
+    source/convert_argb.cc      \
+    source/convert_from.cc      \
+    source/convert_from_argb.cc \
+    source/cpu_id.cc            \
+    source/format_conversion.cc \
+    source/planar_functions.cc  \
+    source/rotate.cc            \
+    source/rotate_argb.cc       \
+    source/row_common.cc        \
+    source/row_mips.cc          \
+    source/row_posix.cc         \
+    source/scale.cc             \
+    source/scale_argb.cc        \
+    source/scale_mips.cc        \
+    source/video_common.cc      \
 
 # TODO(fbarchard): Enable mjpeg encoder.
 #   source/mjpeg_decoder.cc     
