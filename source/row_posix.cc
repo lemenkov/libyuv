1fb68cda67be73f8e573a163add7172ca7dfdd9f
fbarchard@google.com <fbarchard@google.com>
2014-09-30 00:39:41 +0000

    port/fix CopyRow_AVX to gcc BUG=363 TESTED=osx build R=tpsiaki@google.com
    Review URL: https://webrtc-codereview.appspot.com/28619004
    
    git-svn-id: http://libyuv.googlecode.com/svn/trunk@1098 16f28f9a-4ce2-e073-06de-1de4eb20be90

--- source/row_posix.cc
+++ source/row_posix.cc
@@ -3271,11 +3271,11 @@ void CopyRow_AVX(const uint8* src, uint8* dst, int count) {
   asm volatile (
     LABELALIGN
   "1:                                          \n"
-    "movdqa    " MEMACCESS(0) ",%%ymm0         \n"
-    "movdqa    " MEMACCESS2(0x20,0) ",%%ymm1   \n"
+    "vmovdqu   " MEMACCESS(0) ",%%ymm0         \n"
+    "vmovdqu   " MEMACCESS2(0x20,0) ",%%ymm1   \n"
     "lea       " MEMLEA(0x40,0) ",%0           \n"
-    "movdqa    %%ymm0," MEMACCESS(1) "         \n"
-    "movdqa    %%ymm1," MEMACCESS2(0x20,1) "   \n"
+    "vmovdqu   %%ymm0," MEMACCESS(1) "         \n"
+    "vmovdqu   %%ymm1," MEMACCESS2(0x20,1) "   \n"
     "lea       " MEMLEA(0x40,1) ",%1           \n"
     "sub       $0x40,%2                        \n"
     "jg        1b                              \n"
