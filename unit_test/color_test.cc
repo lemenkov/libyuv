0494ffee81bffdf4a175fd60ab0a9f71baa537e1
fbarchard@google.com <fbarchard@google.com>
2015-02-02 21:06:51 +0000

    use lrintf to round from float to int instead of round and then cast. BUG=393 TESTED=local windows test passed. R=harryjin@google.com
    Review URL: https://webrtc-codereview.appspot.com/37899004
    
    git-svn-id: http://libyuv.googlecode.com/svn/trunk@1257 16f28f9a-4ce2-e073-06de-1de4eb20be90

--- unit_test/color_test.cc
+++ unit_test/color_test.cc
@@ -8,7 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include <math.h>  // For round
+#include <math.h>  // For lrintf
 #include <stdlib.h>
 
 #include "libyuv/convert.h"
--- unit_test/convert_test.cc
+++ unit_test/convert_test.cc
@@ -8,6 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#include <math.h>  // For lrintf
 #include <stdlib.h>
 #include <time.h>
 
@@ -1248,7 +1249,7 @@ TEST_F(libyuvTest, TestYToARGB) {
   uint8 expectedg[32];
   for (int i = 0; i < 32; ++i) {
     y[i] = i * 5 + 17;
-    expectedg[i] = round((y[i] - 16) * 1.164);
+    expectedg[i] = lrintf((y[i] - 16) * 1.164);
   }
   uint8 argb[32 * 4];
   YToARGB(y, 0, argb, 0, 32, 1);
