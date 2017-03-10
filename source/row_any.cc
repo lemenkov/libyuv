d59d3fcd1810e40745db7fb95417c1ba1909e4a5
Frank Barchard <fbarchard@google.com>
2017-03-10 15:18:04 -0800

    Change parameter for '_Any' functions to param to avoid misnomer
    BUG=None
    TEST=None
    
    Change-Id: I6940fc4753783afd25f83868635381bf801c65f5
    Reviewed-on: https://chromium-review.googlesource.com/452962
    Reviewed-by: Frank Barchard <fbarchard@google.com>
    Commit-Queue: Frank Barchard <fbarchard@google.com>

--- source/row_any.cc
+++ source/row_any.cc
@@ -654,18 +654,18 @@ ANY11B(ARGBCopyYToAlphaRow_Any_SSE2, ARGBCopyYToAlphaRow_SSE2, 0, 1, 4, 7)
 #undef ANY11B
 
 // Any 1 to 1 with parameter.
-#define ANY11P(NAMEANY, ANY_SIMD, T, SBPP, BPP, MASK)                         \
-  void NAMEANY(const uint8* src_ptr, uint8* dst_ptr, T shuffler, int width) { \
-    SIMD_ALIGNED(uint8 temp[64 * 2]);                                         \
-    memset(temp, 0, 64); /* for msan */                                       \
-    int r = width & MASK;                                                     \
-    int n = width & ~MASK;                                                    \
-    if (n > 0) {                                                              \
-      ANY_SIMD(src_ptr, dst_ptr, shuffler, n);                                \
-    }                                                                         \
-    memcpy(temp, src_ptr + n * SBPP, r * SBPP);                               \
-    ANY_SIMD(temp, temp + 64, shuffler, MASK + 1);                            \
-    memcpy(dst_ptr + n * BPP, temp + 64, r * BPP);                            \
+#define ANY11P(NAMEANY, ANY_SIMD, T, SBPP, BPP, MASK)                      \
+  void NAMEANY(const uint8* src_ptr, uint8* dst_ptr, T param, int width) { \
+    SIMD_ALIGNED(uint8 temp[64 * 2]);                                      \
+    memset(temp, 0, 64); /* for msan */                                    \
+    int r = width & MASK;                                                  \
+    int n = width & ~MASK;                                                 \
+    if (n > 0) {                                                           \
+      ANY_SIMD(src_ptr, dst_ptr, param, n);                                \
+    }                                                                      \
+    memcpy(temp, src_ptr + n * SBPP, r * SBPP);                            \
+    ANY_SIMD(temp, temp + 64, param, MASK + 1);                            \
+    memcpy(dst_ptr + n * BPP, temp + 64, r * BPP);                         \
   }
 
 #if defined(HAS_ARGBTORGB565DITHERROW_SSE2)
@@ -718,19 +718,18 @@ ANY11P(ARGBShuffleRow_Any_MSA, ARGBShuffleRow_MSA, const uint8*, 4, 4, 7)
 #undef ANY11P
 
 // Any 1 to 1 with parameter and shorts.  BPP measures in shorts.
-#define ANY11P16(NAMEANY, ANY_SIMD, T, SBPP, BPP, MASK)            \
-  void NAMEANY(const uint16* src_ptr, uint16* dst_ptr, T shuffler, \
-               int width) {                                        \
-    SIMD_ALIGNED(uint16 temp[16 * 2]);                             \
-    memset(temp, 0, 32); /* for msan */                            \
-    int r = width & MASK;                                          \
-    int n = width & ~MASK;                                         \
-    if (n > 0) {                                                   \
-      ANY_SIMD(src_ptr, dst_ptr, shuffler, n);                     \
-    }                                                              \
-    memcpy(temp, src_ptr + n, r * SBPP);                           \
-    ANY_SIMD(temp, temp + 16, shuffler, MASK + 1);                 \
-    memcpy(dst_ptr + n, temp + 16, r * BPP);                       \
+#define ANY11P16(NAMEANY, ANY_SIMD, T, SBPP, BPP, MASK)                      \
+  void NAMEANY(const uint16* src_ptr, uint16* dst_ptr, T param, int width) { \
+    SIMD_ALIGNED(uint16 temp[16 * 2]);                                       \
+    memset(temp, 0, 32); /* for msan */                                      \
+    int r = width & MASK;                                                    \
+    int n = width & ~MASK;                                                   \
+    if (n > 0) {                                                             \
+      ANY_SIMD(src_ptr, dst_ptr, param, n);                                  \
+    }                                                                        \
+    memcpy(temp, src_ptr + n, r * SBPP);                                     \
+    ANY_SIMD(temp, temp + 16, param, MASK + 1);                              \
+    memcpy(dst_ptr + n, temp + 16, r * BPP);                                 \
   }
 
 #ifdef HAS_HALFFLOATROW_SSE2
