8b8f872dd7926842781996b176b7441acc59a5d6
Oleh Prypin <oprypin@webrtc.org>
2018-04-11 09:57:08 +0200

    Migrate autoroller from roll-dep-svn to gclient setdep
    Bug: webrtc:9104
    No-Try: True
    Change-Id: I3860b0f7b07fa5110a0bc552965c49801c7a3acd
    Reviewed-on: https://chromium-review.googlesource.com/1005002
    Commit-Queue: Oleh Prypin <oprypin@chromium.org>
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>

--- tools_libyuv/autoroller/roll_deps.py
+++ tools_libyuv/autoroller/roll_deps.py
@@ -345,11 +345,9 @@ def UpdateDepsFile(deps_filename, old_cr_revision, new_cr_revision,
           'contains all platforms in the target_os list, i.e.\n'
           'target_os = ["android", "unix", "mac", "ios", "win"];\n'
           'Then run "gclient sync" again.' % local_dep_dir)
-    _, stderr = _RunCommand(
-      ['roll-dep-svn', '--no-verify-revision', dep.path, dep.new_rev],
-      working_dir=CHECKOUT_SRC_DIR, ignore_exit_code=True)
-    if stderr:
-      logging.warning('roll-dep-svn: %s', stderr)
+    _RunCommand(
+      ['gclient', 'setdep', '--revision', '%s@%s' % (dep.path, dep.new_rev)],
+      working_dir=CHECKOUT_SRC_DIR)
 
 
 def _IsTreeClean():
