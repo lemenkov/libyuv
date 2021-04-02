9c1493a6d78c772af8fe0d0651df44fe1c7488cb
Mirko Bonadei <mbonadei@chromium.org>
2021-04-02 20:55:14 +0200

    Remove running valgrind tests at presubmit time.
    TBR=fbarchard@chromium.org
    No-Try: True
    Bug: libyuv:892
    Change-Id: Ic65a8ea7e6b3293843af3d20988272e3ca7aba25
    Reviewed-on: https://chromium-review.googlesource.com/c/libyuv/libyuv/+/2803132
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -6,28 +6,6 @@
 # in the file PATENTS. All contributing project authors may
 # be found in the AUTHORS file in the root of the source tree.
 
-import os
-
-
-def _RunPythonTests(input_api, output_api):
-  def join(*args):
-    return input_api.os_path.join(input_api.PresubmitLocalPath(), *args)
-
-  test_directories = [
-      root for root, _, files in os.walk(join('tools_libyuv'))
-      if any(f.endswith('_test.py') for f in files)
-  ]
-
-  tests = []
-  for directory in test_directories:
-    tests.extend(
-      input_api.canned_checks.GetUnitTestsInDirectory(
-          input_api,
-          output_api,
-          directory,
-          allowlist=[r'.+_test\.py$']))
-  return input_api.RunTests(tests, parallel=True)
-
 
 def _CommonChecks(input_api, output_api):
   """Checks common to both upload and commit."""
@@ -49,7 +27,6 @@ def _CommonChecks(input_api, output_api):
                          'W0232',  # Class has no __init__ method
                         ],
       pylintrc='pylintrc'))
-  results.extend(_RunPythonTests(input_api, output_api))
   return results
 
 
