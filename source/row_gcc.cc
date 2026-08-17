d67b51303dae90f3b3d184e42a990e674481ea1e
Wan-Teh Chang <wtc@google.com>
2026-08-17 14:26:37 -0700

    Revert "Add the ARGBToRGB565Row_SSE2() function back"
    This reverts commit 87c797b3c29cc6fb233fc329a93aace2942ba2d9.
    
    Reason for revert: Only commit 8aeb3a9 needs the workaround
    
    Original change's description:
    > Add the ARGBToRGB565Row_SSE2() function back
    >
    > Add the ARGBToRGB565Row_SSE2() and ARGBToRGB565Row_Any_SSE2() functions
    > back because they hide the unaligned access in ARGBToRGB565Row_C().
    >
    > To reproduce the unalgined access error, run the following command in
    > Google's internal repository:
    >   blaze test --config=asan third_party/libyuv:libyuv_test \
    >   --test_filter=LibYUVConvertTest.I420ToARGBToRGB565_Unaligned
    >
    > Bug: b:547803993
    > Change-Id: I9cc6b643bc007c100dfc092ef3ec814428789219
    > Reviewed-on: https://chromium-review.googlesource.com/c/libyuv/libyuv/+/8265046
    > Commit-Queue: Wan-Teh Chang <wtc@google.com>
    > Reviewed-by: Frank Barchard <fbarchard@google.com>
    
    Bug: b:547803993
    Change-Id: I904126f032f4ed37c470d8972152a137ca315168
    Reviewed-on: https://chromium-review.googlesource.com/c/libyuv/libyuv/+/8258919
    Reviewed-by: Frank Barchard <fbarchard@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>

--- source/row_gcc.cc
+++ source/row_gcc.cc
@@ -875,43 +875,6 @@ void ARGBToRAWRow_AVX2(const uint8_t* src, uint8_t* dst, int width) {
 }
 #endif
 
-void ARGBToRGB565Row_SSE2(const uint8_t* src, uint8_t* dst, int width) {
-  asm volatile(
-      "pcmpeqb     %%xmm3,%%xmm3                 \n"
-      "psrld       $0x1b,%%xmm3                  \n"
-      "pcmpeqb     %%xmm4,%%xmm4                 \n"
-      "psrld       $0x1a,%%xmm4                  \n"
-      "pslld       $0x5,%%xmm4                   \n"
-      "pcmpeqb     %%xmm5,%%xmm5                 \n"
-      "pslld       $0xb,%%xmm5                   \n"
-
-      LABELALIGN
-      "1:          \n"
-      "movdqu      (%0),%%xmm0                   \n"
-      "movdqa      %%xmm0,%%xmm1                 \n"
-      "movdqa      %%xmm0,%%xmm2                 \n"
-      "pslld       $0x8,%%xmm0                   \n"
-      "psrld       $0x3,%%xmm1                   \n"
-      "psrld       $0x5,%%xmm2                   \n"
-      "psrad       $0x10,%%xmm0                  \n"
-      "pand        %%xmm3,%%xmm1                 \n"
-      "pand        %%xmm4,%%xmm2                 \n"
-      "pand        %%xmm5,%%xmm0                 \n"
-      "por         %%xmm2,%%xmm1                 \n"
-      "por         %%xmm1,%%xmm0                 \n"
-      "packssdw    %%xmm0,%%xmm0                 \n"
-      "lea         0x10(%0),%0                   \n"
-      "movq        %%xmm0,(%1)                   \n"
-      "lea         0x8(%1),%1                    \n"
-      "sub         $0x4,%2                       \n"
-      "jg          1b                            \n"
-      : "+r"(src),   // %0
-        "+r"(dst),   // %1
-        "+r"(width)  // %2
-        ::"memory",
-        "cc", "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5");
-}
-
 #ifdef HAS_ARGBTORGB565DITHERROW_AVX2
 void ARGBToRGB565DitherRow_AVX2(const uint8_t* src,
                                 uint8_t* dst,
